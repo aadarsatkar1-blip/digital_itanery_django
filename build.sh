98fcca7b484b966ede3e886ed217e109ed147921
[company system] <[aadarsatkar1@gmail.com]>
2025-12-29 15:46:06 +0530

    add tailwindcss build commond

--- build.sh
+++ build.sh
@@ -1,9 +1,21 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
 
+# install python deps
 pip install -r requirements.txt
 
+# install node deps
+npm install
+
+# build tailwind css
+npx tailwindcss -i ./static/css/input.css -o ./static/css/output.css --minify
+
+# collect static files
 python manage.py collectstatic --no-input
+
+# migrate database
 python manage.py migrate
+
+# create superuser if not exists
 python manage.py create_superuser_if_none
+
