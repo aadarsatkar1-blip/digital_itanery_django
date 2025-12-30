df1e89512d1a34e14e47221286909c15835d9f6e
[company system] <[aadarsatkar1@gmail.com]>
2025-12-30 10:47:30 +0530

    Use npm build script for production CSS compilation

--- build.sh
+++ build.sh
@@ -3,9 +3,10 @@ set -o errexit
 
 pip install -r requirements.txt
 npm install
-npx tailwindcss -i ./static/css/input.css -o ./static/css/output.css --minify
+npm run build
 python manage.py collectstatic --no-input
 python manage.py migrate
 python manage.py create_superuser_if_none
+
 
 
